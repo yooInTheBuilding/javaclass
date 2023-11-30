e52e11707295c9e031cccba20b52396265885566
yooInTheBuilding <dbals9926@gmail.com>
2023-11-30 18:10:34 +0900

    after_class

--- src/ch02_operator/Ex01_Arithmeticoperator.java
+++ src/ch02_operator/Ex01_Arithmeticoperator.java
@@ -3,9 +3,8 @@ package ch02_operator;
 public class Ex01_Arithmeticoperator {
 
 	public static void main(String[] args) {
-		int num1 = 50;
-		int num2 = 25;
-		System.out.println((num1+num2) + "\n" + (num1-num2) + "\n" + (num1*num2) + "\n" + (num1/num2) + "\n" + (num1%num2));
+		double var1 = 10.0/3;
+		System.out.println(var1);
 	}
 
 }
