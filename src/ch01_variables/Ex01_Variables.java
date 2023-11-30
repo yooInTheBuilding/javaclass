7f0d7985d4c16d6e95ba697bf469228b20a789b8
yooInTheBuilding <dbals9926@gmail.com>
2023-11-30 14:25:12 +0900

    variables_class_v1

---
+++ src/ch01_variables/Ex01_Variables.java
@@ -0,0 +1,15 @@
+package ch01_variables;
+
+public class Ex01_Variables {
+
+	public static void main(String[] args) {
+		// 본인의 이름, 나이, 주소, 전화번호를 입력할 수 있는 변수를 각각 선언
+		String name = "유시훈";
+		int age = 22;
+		double height = 182.7;
+		String adress = "논현동";
+		String mobile = "010-7617-4361";
+		System.out.println(name + "\n" + age + "\n" + height + "\n" + adress + "\n" + mobile);
+	}
+
+}
