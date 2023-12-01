4c516ee93be54a8c2b75bf83880e4f1f1a618cae
yooInTheBuilding <dbals9926@gmail.com>
2023-12-01 13:15:47 +0900

    scanner_class_over

--- src/ch03_scanner/Calculator.java
+++ src/ch03_scanner/Calculator.java
@@ -6,12 +6,23 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("숫자를 입력하세요.: ");
+		System.out.print("숫자를 입력하세요.: ");
 		int num1 = sc.nextInt();
-		System.out.println("\n숫자를 입력하세요.: ");
+		System.out.print("\n숫자를 입력하세요.: ");
 		int num2 = sc.nextInt();
+		
+		System.out.print("이름을 입력하세요: ");
+		String name = sc.next();
+		System.out.println(name);
+		
+		sc.nextLine();
+		
+		System.out.print("주소입력: ");
+		String adress = sc.nextLine();
+		System.out.print(adress);
 		int result = num1 + num2;
 		System.out.println(num1 + num2);
+		
 	}
 
 }
